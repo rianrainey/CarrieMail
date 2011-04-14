8e2c6d147910e81119c822ff351f22bc7774b5d0
Mark Harris <ma_harris1011@hotmail.com>
2011-04-13 23:34:35 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CarrieMail::Application.config.secret_token = 'b5371cfbfd643c6996de94584dc488c544bb7d4ece5abe371a38df56e548c6f8e09dc666aaa2f4e22a7cc962bc0a80495c9495651007a103532be0f6a7d01f52'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CarrieMail::Application.config.session_store :cookie_store, :key => '_CarrieMail_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CarrieMail::Application.config.session_store :active_record_store
