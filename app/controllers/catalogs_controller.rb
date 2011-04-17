738b497b1eee1a4bbb385049b56c9380711f3fee
Mark Harris <ma_harris1011@hotmail.com>
2011-04-16 22:32:55 -0400

    switching for now

--- app/controllers/catalogs_controller.rb
+++ app/controllers/catalogs_controller.rb
@@ -1,2 +1,3 @@
 class CatalogsController < ApplicationController
+  
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,12 @@
 class HomeController < ApplicationController
   def index
     @user = current_user || User.new
+    if user_signed_in?
+      @catalog = Catalog.new()
+      redirect_to catalog_notes_path(@catalog)
+    else
+      redirect_to new_user_session_path
+    end
   end
 
 end
