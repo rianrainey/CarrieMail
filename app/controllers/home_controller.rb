b73a1b69f244851d573d8c809ac59491b32b260f
Mark Harris <mark.harris1011@gmail.com>
2011-08-16 21:14:44 -0400

    re-use the forms from our current setup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,15 @@ class HomeController < ApplicationController
   
   def index
     @title = 'Home'
+    
     @guest_user = User.find_by_first_name("guest")
     if @guest_user.nil?
-      @guest_user = User.create(:first_name=>'guest',:last_name=>'guest',:email=>'guest@carriemail.com',:password=>'guest123')
+      @guest_user = User.create(:first_name=>'guest',:last_name=>'guest',
+                                :email=>'guest@carriemail.com',:password=>'guest123')
     end
     
     sign_in(:user, @guest_user)
+    @catalog = current_user.catalog
     
     respond_to do |format| 
       format.html #index.html.erb
